5c03f1482b336c567c17da91362ebf263ed77abb
dat_dm_vn <datdm@ascendsgroup.com>
2021-08-18 16:09:33 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import { SignUpModal } from "./components";
+import { GlobalStyle, darkTheme, defaultTheme } from "./utils";
+
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default theme
+      </button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setShowModal(!showModal)}
+      >
+        Toggle modal
+      </button>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around"
+        }}
+      >
+        <SignUpModal showModal={showModal} setShowModal={setShowModal} />
+        <GlobalStyle />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
